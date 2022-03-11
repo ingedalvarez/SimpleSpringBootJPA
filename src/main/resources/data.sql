3ae4f72aee11f23520ba439027c3db3b9a5ac37b
ingedalvarez <ingedalvarez@gmail.com>
2022-03-11 14:53:56 -0500

    creacion del archivo data.sql popular base de datos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO employee (id, name, age, salary) VALUES (101, 'Edward Alvarez', 44, 1200);
+INSERT INTO employee (id, name, age, salary) VALUES (102, 'Freddy Alvarez', 41, 1300);
+INSERT INTO employee (id, name, age, salary) VALUES (103, 'Johan Alvarez', 47, 1100);
+INSERT INTO employee (id, name, age, salary) VALUES (104, 'Patricia Alvarez', 38, 1000);
+INSERT INTO employee (id, name, age, salary) VALUES (105, 'Yolecy Sarmiento', 37, 1800);
+INSERT INTO employee (id, name, age, salary) VALUES (106, 'Carmen Gonzalez', 65, 1900);
+INSERT INTO employee (id, name, age, salary) VALUES (107, 'Yalena Sarmiento', 43, 2200);
+INSERT INTO employee (id, name, age, salary) VALUES (108, 'Raul Sarmiento', 43, 3000);
+INSERT INTO employee (id, name, age, salary) VALUES (109, 'Veronica Viedma', 35, 2500);
+
---
+++ src/test/java/org/ingedalvarez/controller/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package org.ingedalvarez.controller;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeControllerTest {
+
+	@Test
+	void testHome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testShowEmployee() {
+		
+		fail("Not yet implemented");
+	}
+
+}
